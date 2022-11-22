560285ffdce80578920b4894308528aa393f5e8d
Marco Rodrigues <rmarco@umich.edu>
2022-11-22 15:07:16 -0500

    Experiment while working through part 1.a

--- part1/sandbox/part1/src/App.js
+++ part1/sandbox/part1/src/App.js
@@ -1,9 +1,25 @@
+const Hello = (props) => {
+  return (
+    <>
+      <p>
+        Hello {props.name}, you are {props.age} years old.
+      </p>
+    </>
+  );
+};
 
+const App = () => {
+  const name = "Raph";
+  const age = 24;
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Bryce" age = {age - 1}/>
+      <Hello name="Gabe" age = {20 + 3}/>
+      <Hello name={name} age = {age}/>
+    </>
+  );
+};
 
 export default App;
