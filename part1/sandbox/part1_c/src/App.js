404eb75c31a52616b6bf059a05eb6b526e6554da
Marco Rodrigues <rmarco@umich.edu>
2022-12-01 20:32:41 -0500

    Move duplicate code into separate components following along with part 1.c

--- part1/sandbox/part1_c/src/App.js
+++ part1/sandbox/part1_c/src/App.js
@@ -1,8 +1,43 @@
-const App = (props) => {
-  const {counter} = props;
-  return (
-    <>{counter}</>
-  );
+import {useState} from "react";
+import Display from "./Display";
+import Button from "./Button";
+
+const App = () => {
+
+    const [counter, setCounter] = useState(0);
+
+    /*
+    setTimeout(
+    () => setCounter(counter + 1),
+    1000
+    );
+
+    console.log("rendering...", counter);
+    */
+
+    const increaseByOne = () => setCounter(counter + 1);
+
+    const setToZero = () => setCounter(0);
+
+    const decreaseByOne = () => setCounter(counter - 1);
+
+    return (
+        <>
+            <Display counter={counter} />
+            <Button 
+                onClick={increaseByOne} 
+                text="add"
+            />
+            <Button 
+                onClick={setToZero}
+                text="reset"
+            />
+            <Button 
+                onClick={decreaseByOne}
+                text="subtract"
+            />
+        </>
+    );
 };
 
 export default App;
--- part1/sandbox/part1_c/src/index.js
+++ part1/sandbox/part1_c/src/index.js
@@ -1,33 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
+import App from "./App";
 
-let counter = 1;
-
-const refresh = () => {
-    ReactDOM.createRoot(document.getElementById('root')).render(
-        <App counter={counter}/>
-    );
-};
-
-/*
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter}/>
-);
-*/
-
-/*
-refresh();
-counter += 1;
-refresh();
-counter += 1;
-refresh();
-*/
-
-/*
-setInterval(() => {
-    refresh();
-    counter += 1;
-}, 1000);
-*/
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
