83f997610122cf6997a75cd03e00bbac5188a742
Marco Rodrigues <rmarco@umich.edu>
2022-12-10 15:50:36 -0500

    Experiment with controlled components in part 2.b

--- part2/sandbox/render_collection/src/App.js
+++ part2/sandbox/render_collection/src/App.js
@@ -1,14 +1,47 @@
+import {useState} from "react";
 import Note from "./components/Note.js";
 
-const App = ({notes}) => {
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes);
+    const [newNote, setNewNote] = useState("");
+    const [showAll, setShowAll] = useState(true);
+
+    const addNote = (event) => {
+        event.preventDefault();
+        const noteToAdd = {
+            id: notes.length + 1,
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() < 0.5
+        };
+        setNotes(notes.concat(noteToAdd));
+        setNewNote("");
+    };
+
+    const handleNoteChange = (event) => {
+        setNewNote(event.target.value);
+    };
+
+    const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important);
+
+
     return (
         <>
             <h1>Notes</h1>
+            <button onClick={() => setShowAll(!showAll)}>
+                show {showAll ? "important" : "all"}
+            </button>
             <ul>
-                {notes.map(note =>
+                {notesToShow.map(note =>
                     <Note key={note.id} text={note.content} />
                 )}
             </ul>
+            <form onSubmit={addNote}>
+                <input value={newNote} onChange={handleNoteChange}/>
+                <button type="submit">save</button>
+            </form>
         </>
     );
 };
