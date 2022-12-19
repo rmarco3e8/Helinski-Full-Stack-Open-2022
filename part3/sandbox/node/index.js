9086f5866dd4ddf3361bfc584a90344909b5cb97
Marco Rodrigues <rmarco@umich.edu>
2022-12-19 10:09:38 -0500

    Experiment with middleware in part 3.a

--- part3/sandbox/node/index.js
+++ part3/sandbox/node/index.js
@@ -24,6 +24,16 @@ let notes = [
         }
 ];
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method);
+    console.log('Path:  ', request.path);
+    console.log('Body:  ', request.body);
+    console.log('---');
+    next();
+};
+
+app.use(requestLogger);
+
 app.get("/", (request, response) => {
     response.send("<h1>Hello World!</h2>");
 })
@@ -78,6 +88,12 @@ app.post("/api/notes", (request, response) => {
 
     response.json(note);
 });
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: "unknown endpoint"});
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 
