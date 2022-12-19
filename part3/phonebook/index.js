be8d711fdb4bfef89f1230b55218733be62902af
Marco Rodrigues <rmarco@umich.edu>
2022-12-19 10:41:41 -0500

    Complete exercises 3.7-3.8 (morgan)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,19 @@
-const express = require ("express");
+const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
+
+morgan.token("post-data", (req, res) => {
+    if(req.method === "POST") {
+        return JSON.stringify(req.body);
+    } else {
+        return "";
+    }
+});
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :post-data"));
 
 let persons = [
     { 
