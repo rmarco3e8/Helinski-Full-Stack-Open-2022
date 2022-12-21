5fdc9a85b92c9a23b843eec81a6d18203d9e6aff
Marco Rodrigues <rmarco@umich.edu>
2022-12-21 12:06:49 -0500

    Connect backend for note service to MongoDB, fix GET and POST endpoints in part 3.c

--- part3/sandbox/node/index.js
+++ part3/sandbox/node/index.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const Note = require("./models/note");
 
 const app = express();
 
@@ -43,18 +46,20 @@ app.get("/", (request, response) => {
 })
 
 app.get("/api/notes", (request, response) => {
-    response.json(notes);
+    Note.find({}).then((notes) => {
+        response.json(notes);
+    });
 });
 
 app.get("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(note => note.id === id);
 
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    Note.findById(request.params.id)
+        .then(foundNote => {
+            response.json(foundNote);
+        })
+        .catch(err => {
+            response.status(404).end();
+        });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -64,33 +69,29 @@ app.delete("/api/notes/:id", (request, response) => {
     response.status(204).end();
 });
 
-const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(note => note.id))
-    : 0;
-    return maxId + 1;
-};
+// const generateId = () => {
+//     const maxId = notes.length > 0
+//     ? Math.max(...notes.map(note => note.id))
+//     : 0;
+//     return maxId + 1;
+// };
 
 app.post("/api/notes", (request, response) => {
-
     const body = request.body;
 
     if (!body.content) {
-        return response.status(400).json({
-            error: "content missing"
-        });
+        return response.status(400).json({ error: "content missing" });
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId()
-    };
+        date: new Date()
+    });
 
-    notes = notes.concat(note);
-
-    response.json(note);
+    note.save().then(savedNote => {
+        response.json(savedNote);
+    });
 });
 
 const unknownEndpoint = (request, response) => {
@@ -99,7 +100,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
