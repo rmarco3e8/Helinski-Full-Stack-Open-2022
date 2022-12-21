9b7281e845f126486bbd21d8a5b52dbeb42afb46
Marco Rodrigues <rmarco@umich.edu>
2022-12-21 15:51:37 -0500

    Add error handling middleware, fix PUT and DELETE routes in part 3.c

--- part3/sandbox/node/index.js
+++ part3/sandbox/node/index.js
@@ -50,22 +50,25 @@ app.get("/api/notes", (request, response) => {
     });
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
 
     Note.findById(request.params.id)
         .then(foundNote => {
-            response.json(foundNote);
+            if (foundNote) {
+                response.json(foundNote);
+            } else {
+                response.status(404).end();
+            }
         })
-        .catch(err => {
-            response.status(404).end();
-        });
+        .catch(error => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-
-    response.status(204).end();
+app.delete("/api/notes/:id", (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
 // const generateId = () => {
@@ -93,11 +96,39 @@ app.post("/api/notes", (request, response) => {
     });
 });
 
+app.put("/api/notes/:id", (request, response, next) => {
+
+    const body = request.body;
+    
+    const note = {
+        content: body.content,
+        important: body.important
+    };
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote);
+        })
+        .catch(error => next(error));
+});
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({error: "unknown endpoint"});
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === "CastError") {
+        return response.status(400).send({error: "malformatted id"});
+    }
+
+    next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
