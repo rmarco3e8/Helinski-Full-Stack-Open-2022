0c499a0cc0ec662ddd68856ca615370a2a143989
Marco Rodrigues <rmarco@umich.edu>
2022-12-22 12:15:11 -0500

    Experiment with Mongoose validation in part 3.d (note service)

--- part3/sandbox/node/index.js
+++ part3/sandbox/node/index.js
@@ -78,12 +78,8 @@ app.delete("/api/notes/:id", (request, response, next) => {
 //     return maxId + 1;
 // };
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
     const body = request.body;
-
-    if (!body.content) {
-        return response.status(400).json({ error: "content missing" });
-    }
 
     const note = new Note({
         content: body.content,
@@ -93,19 +89,18 @@ app.post("/api/notes", (request, response) => {
 
     note.save().then(savedNote => {
         response.json(savedNote);
-    });
+    })
+    .catch(error => next(error));
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
+    const {content, important} = request.body;
 
-    const body = request.body;
-    
-    const note = {
-        content: body.content,
-        important: body.important
-    };
-
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(
+        request.params.id,
+        {content, important}, 
+        {new: true, runValidators: true, context: "query"}
+    )
         .then(updatedNote => {
             response.json(updatedNote);
         })
@@ -123,6 +118,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === "CastError") {
         return response.status(400).send({error: "malformatted id"});
+    } else if (error.name === "ValidationError") {
+        return response.status(400).json({error: error.message});
     }
 
     next(error);
