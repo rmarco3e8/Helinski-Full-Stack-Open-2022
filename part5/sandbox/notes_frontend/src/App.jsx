a9666cdff400327f96c1ddf7d531fb2c10a97156
Marco Rodrigues <rmarco@umich.edu>
2023-01-04 13:38:42 -0500

    Add user auth, refactor forms, fix creating notes in part 5.a (notes frontend)

--- part5/sandbox/notes_frontend/src/App.jsx
+++ part5/sandbox/notes_frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { React, useState, useEffect } from 'react';
 import Note from './components/Note';
 import Notification from './components/Notification';
-
+import LoginForm from './components/LoginForm';
+import NoteForm from './components/NoteForm';
 import noteService from './services/notes';
-
+import loginService from './services/login';
 import './index.css';
 
 const Footer = () => {
@@ -27,6 +28,7 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState(null);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     noteService
@@ -52,10 +54,6 @@ const App = () => {
       });
   };
 
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value);
-  };
-
   const toggleImportance = (id) => {
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
@@ -75,9 +73,25 @@ const App = () => {
     ? notes
     : notes.filter((note) => note.important);
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
-    console.log('logging in with', username, password);
+
+    try {
+      const newUser = await loginService.login({
+        username, password,
+      });
+      noteService.setToken(newUser.token);
+      setUser(newUser);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      setErrorMessage('Wrong credentials');
+      setTimeout(() => setErrorMessage(null), 5000);
+    }
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
   };
 
   return (
@@ -86,30 +100,29 @@ const App = () => {
 
       <Notification message={errorMessage} />
 
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
+      {user === null
+        ? (
+          <LoginForm
+            handleLogin={handleLogin}
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
           />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
+        )
+        : (
+          <div>
+            <p>{`${user.name} logged-in`}</p>
+            <NoteForm
+              addNote={addNote}
+              newNote={newNote}
+              handleNoteChange={handleNoteChange}
+            />
+          </div>
+        )}
 
       <button type="button" onClick={() => setShowAll(!showAll)}>
-        show
+        show&nbsp;
         { showAll ? 'important' : 'all' }
       </button>
 
@@ -119,10 +132,6 @@ const App = () => {
         ))}
       </ul>
 
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
       <Footer />
     </>
   );
