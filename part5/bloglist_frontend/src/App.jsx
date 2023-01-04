5e4df0cb8df639de0a781be9762daf48953156e0
Marco Rodrigues <rmarco@umich.edu>
2023-01-04 18:20:56 -0500

    Complete exercises 5.1-5.2 (login form, browser local storage, bloglist frontend)

--- part5/bloglist_frontend/src/App.jsx
+++ part5/bloglist_frontend/src/App.jsx
@@ -1,19 +1,77 @@
 import { React, useState, useEffect } from 'react';
 import Blog from './components/Blog';
+import LoginForm from './components/LoginForm';
 import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then((b) => setBlogs(b));
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
+    if (loggedUserJSON) {
+      const loggedUser = JSON.parse(loggedUserJSON);
+      setUser(loggedUser);
+      blogService.setToken(loggedUser.token);
+    }
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const newUser = await loginService.login(
+        { username, password },
+      );
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(newUser));
+      blogService.setToken(newUser.token);
+      setUser(newUser);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      console.log('Wrong credentials');
+    }
+  };
+
+  const handleLogout = (event) => {
+    event.preventDefault();
+
+    window.localStorage.removeItem('loggedBlogappUser');
+    blogService.setToken(null);
+    setUser(null);
+  };
+
   return (
-    <div>
+    <>
       <h2>blogs</h2>
-      {blogs.map((b) => <Blog key={b.id} blog={b} />)}
-    </div>
+      {user === null
+        ? (
+          <LoginForm
+            handleLogin={handleLogin}
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+          />
+        )
+        : (
+          <div>
+            <p>
+              {`${user.name} logged in`}
+              &nbsp;
+              <button type="button" onClick={handleLogout}>logout</button>
+            </p>
+            {blogs.map((b) => <Blog key={b.id} blog={b} />)}
+          </div>
+        )}
+    </>
   );
 };
 
