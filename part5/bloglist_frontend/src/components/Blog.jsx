6b132900856a1c5e012b6eb127ea221c707b3d83
Marco Rodrigues <rmarco@umich.edu>
2023-01-05 20:02:40 -0500

    Complete exercise 5.7 (toggle button to show all blog info)

--- part5/bloglist_frontend/src/components/Blog.jsx
+++ part5/bloglist_frontend/src/components/Blog.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import { React, useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    {`${blog.title} ${blog.author}`}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const showWhenVisible = { display: visible ? '' : 'none' };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {`${blog.title} ${blog.author}`}
+        &nbsp;
+        <button type="button" onClick={toggleVisibility}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>{blog.url}</div>
+        <div>
+          {`likes ${blog.likes}`}
+          &nbsp;
+          <button type="button">like</button>
+        </div>
+        <div>{blog.user.username}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
