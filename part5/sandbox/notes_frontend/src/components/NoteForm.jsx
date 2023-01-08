8a1db0e1cb61d76544e35f69be971f58e8ea853d
Marco Rodrigues <rmarco@umich.edu>
2023-01-08 15:43:14 -0500

    Experiment with react-testing-library/Jest more in part 5.c

--- part5/sandbox/notes_frontend/src/components/NoteForm.jsx
+++ part5/sandbox/notes_frontend/src/components/NoteForm.jsx
@@ -20,17 +20,18 @@ const NoteForm = ({ createNote }) => {
   };
 
   return (
-    <>
+    <div className="formDiv">
       <h2>Create a new note</h2>
 
       <form onSubmit={addNote}>
         <input
           value={newNote}
           onChange={handleNoteChange}
+          placeholder="write note content here"
         />
         <button type="submit">save</button>
       </form>
-    </>
+    </div>
   );
 };
 
