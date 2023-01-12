08a0307e8dd1618e860aeccc4e7be3755da773b1
Marco Rodrigues <rmarco@umich.edu>
2023-01-12 15:11:43 -0500

    Complete exercises 6.1-6.2 (unicafe redux)

---
+++ part6/unicafe-redux-main/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react', 'jest',
+  ],
+  rules: {
+    'no-console': 0,
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/prop-types': 0,
+  },
+};
