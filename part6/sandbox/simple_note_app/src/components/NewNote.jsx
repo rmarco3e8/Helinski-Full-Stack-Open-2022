a1e5ae443b5a48edad121b1a59d6af1358f1cde4
Marco Rodrigues <rmarco@umich.edu>
2023-01-19 16:23:03 -0500

    Experiment with connect instead of useDispatch, useSelector in part 6.d

--- part6/sandbox/simple_note_app/src/components/NewNote.jsx
+++ part6/sandbox/simple_note_app/src/components/NewNote.jsx
@@ -1,16 +1,41 @@
+// import React from 'react';
+// import { useDispatch } from 'react-redux';
+// import { createNote } from '../reducers/noteReducer';
+
+// const NewNote = () => {
+//   const dispatch = useDispatch();
+
+//   /* eslint-disable no-param-reassign */
+//   const addNote = async (event) => {
+//     event.preventDefault();
+//     const content = event.target.note.value;
+//     event.target.note.value = '';
+//     dispatch(createNote(content));
+//   };
+//   /* eslint-disable no-param-reassign */
+
+//   return (
+//     <form onSubmit={addNote}>
+//       <input name="note" />
+//       <button type="submit">add</button>
+//     </form>
+//   );
+// };
+
+// export default NewNote;
+
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import { createNote } from '../reducers/noteReducer';
 
-const NewNote = () => {
-  const dispatch = useDispatch();
-
+/* eslint-disable react/destructuring-assignment */
+const NewNote = (props) => {
   /* eslint-disable no-param-reassign */
-  const addNote = async (event) => {
+  const addNote = (event) => {
     event.preventDefault();
     const content = event.target.note.value;
     event.target.note.value = '';
-    dispatch(createNote(content));
+    props.createNote(content);
   };
   /* eslint-disable no-param-reassign */
 
@@ -21,5 +46,9 @@ const NewNote = () => {
     </form>
   );
 };
+/* eslint-enable react/destructuring-assignment */
 
-export default NewNote;
+export default connect(
+  null,
+  { createNote },
+)(NewNote);
--- part6/sandbox/simple_note_app/src/components/Notes.jsx
+++ part6/sandbox/simple_note_app/src/components/Notes.jsx
@@ -1,5 +1,44 @@
+/* more modern, simpler way, using the hook API from react-redux */
+// import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { toggleImportanceOf } from '../reducers/noteReducer';
+
+// const Note = ({ note, handleClick }) => (
+//   <li onClick={handleClick}>
+//     {note.content}
+//     <strong> {note.important ? 'important' : ''}</strong>
+//   </li>
+// );
+
+// const Notes = () => {
+//   const dispatch = useDispatch();
+//   const notesToShow = useSelector(({ filter, notes }) => {
+//     if (filter === 'ALL') {
+//       return notes;
+//     }
+//     return filter === 'IMPORTANT'
+//       ? notes.filter((note) => note.important)
+//       : notes.filter((note) => !note.important);
+//   });
+
+//   return (
+//     <ul>
+//       {notesToShow.map((note) => (
+//         <Note
+//           key={note.id}
+//           note={note}
+//           handleClick={() => dispatch(toggleImportanceOf(note.id))}
+//         />
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default Notes;
+
+/* older, more complicated way, using connect to make notes a connected component */
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import { toggleImportanceOf } from '../reducers/noteReducer';
 
 const Note = ({ note, handleClick }) => (
@@ -9,28 +48,38 @@ const Note = ({ note, handleClick }) => (
   </li>
 );
 
-const Notes = () => {
-  const dispatch = useDispatch();
-  const notesToShow = useSelector(({ filter, notes }) => {
-    if (filter === 'ALL') {
-      return notes;
-    }
-    return filter === 'IMPORTANT'
-      ? notes.filter((note) => note.important)
-      : notes.filter((note) => !note.important);
-  });
+/* eslint-disable react/destructuring-assignment */
+const Notes = (props) => (
+  <ul>
+    {props.notes.map((note) => (
+      <Note
+        key={note.id}
+        note={note}
+        handleClick={() => props.toggleImportanceOf(note.id)}
+      />
+    ))}
+  </ul>
+);
+/* eslint-enable react/destructuring-assignment */
 
-  return (
-    <ul>
-      {notesToShow.map((note) => (
-        <Note
-          key={note.id}
-          note={note}
-          handleClick={() => dispatch(toggleImportanceOf(note.id))}
-        />
-      ))}
-    </ul>
-  );
+const mapStateToProps = (state) => {
+  if (state.filter === 'ALL') {
+    return { notes: state.notes };
+  }
+  return {
+    notes: state.filter === 'IMPORTANT'
+      ? state.notes.filter((note) => note.important)
+      : state.notes.filter((note) => !note.important),
+  };
 };
 
-export default Notes;
+const mapDispatchToProps = {
+  toggleImportanceOf,
+};
+
+const ConnectedNotes = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Notes);
+
+export default ConnectedNotes;
