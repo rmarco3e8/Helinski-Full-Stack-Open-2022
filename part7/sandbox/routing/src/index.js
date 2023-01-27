3466b9362bfd44d296cfe846743fde411db37f11
Marco Rodrigues <rmarco@umich.edu>
2023-01-26 19:18:11 -0500

    Experiment with React Bootstrap in part 7.c

--- part7/sandbox/routing/src/index.js
+++ part7/sandbox/routing/src/index.js
@@ -1,5 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
+import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Alert from 'react-bootstrap/Alert'
 
 import {
   BrowserRouter as Router,
@@ -33,13 +37,22 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
+    <Table striped>
+      <tbody>
+        {notes.map((note) => 
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>
+                {note.content}
+              </Link>
+            </td>
+            <td>
+              {note.user}
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
   </div>
 )
 
@@ -66,15 +79,22 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username: <input />
-        </div>
-        <div>
-          password: <input type='password' />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <Form.Control
+          type="text"
+          name="username"
+          />
+          <Form.Label>password:</Form.Label>
+          <Form.Control
+          type="password"
+          />
+          <Button variant="primary" type="submit">
+            login
+          </Button>
+        </Form.Group>
+      </Form>
     </div>
   )
 }
@@ -102,9 +122,14 @@ const App = () => {
   ])
 
   const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
 
   const login = (user) => {
     setUser(user)
+    setMessage(`welcome ${user}`)
+    setTimeout(() => {
+      setMessage(null)
+    }, 10000)
   }
 
   const padding = {
@@ -117,7 +142,12 @@ const App = () => {
     : null
 
   return (
-    <div>
+    <div className="container">
+        {(message && 
+          <Alert variant="success">
+            {message}
+          </Alert>
+        )}
         <div>
           <Link style={padding} to="/">home</Link>
           <Link style={padding} to="/notes">notes</Link>
@@ -147,4 +177,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <App />
   </Router>
-);
+)
